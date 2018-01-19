f415dd52916e1f163a387d60fbf90a7f6138b7dd
0racles <oracleslonge@gmail.com>
2018-01-19 06:44:42 +0000

    added partials for nav and js to add and remove the active class for menu navigation

--- src/main_app.js
+++ src/main_app.js
@@ -7,12 +7,39 @@ var css = require('../src/assets/sass/global.scss'),
 		//fontAwesome = 
 
 $(document).ready(function () {
+// variables declaration and initialization
+ var title = document.getElementsByTagName('TITLE'),
+ 	 nav_item = document.querySelectorAll('.nav-item'),
+ 	 navbar_item = document.querySelectorAll('.navbar--item');
 
-$('.div').css({
-	'height' : '200px',
-	'color' : '#333'
-	});
+ 	 if (title[0].textContent === 'Features') {
+ 	 	remove_class('.nav-item', 'active');
+ 	 	add_class(nav_item[1], 'active');
+ 	 }
+ 	 else if (title[0].textContent === 'Download') {
+ 	 	remove_class('.nav-item', 'active');
+ 	 	add_class(nav_item[2], 'active');	
+ 	 } else if (title[0].textContent === 'Purchase') {
+ 	 	remove_class('.nav-item', 'active');
+ 	 	add_class(nav_item[3], 'active');
+ 	 }  else if (title[0].textContent === 'Partners') {
+ 	 	remove_class('.navbar--item', 'active');
+ 	 	add_class(navbar_item[1], 'active');
+ 	 }  else if (title[0].textContent === 'Contact') {
+ 	 	remove_class('.navbar--item', 'active');
+ 	 	add_class(navbar_item[2], 'active');
+ 	 }
 
+ 	// callback function for adding and removing classes
+ 	function add_class(a, b) {
+ 		$(a).addClass(b);
+ 	}
+
+ 	function remove_class(a, b) {
+ 		$(a).removeClass(b);
+ 	}
+ 
+// animation for timeline display
 $(window).on('scroll', function(){
 	$('.timeline--block').each(function(){
 		if( $(this).offset().top <= $(window).scrollTop()+$(window).height()*0.15 && $(this).find('.timeline--image').hasClass('is-hidden') ) {
@@ -21,6 +48,7 @@ $(window).on('scroll', function(){
 	});
 });
 
+// alerting users to visit the actual site whenever an action button is clicked
 $('.btn').click(function() {
 	alert('visit http://ngwin.com/picpick to perform this action');
 });
